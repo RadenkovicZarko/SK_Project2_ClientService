2c5674cb4ff29d5929c98a2619feb936aeb2adc6
cvele0 <vladancvjetkovic@gmail.com>
2022-12-14 23:43:37 +0100

    Initial commit

---
+++ src/test/java/com/komponente/Korisnicki/KorisnickiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komponente.Korisnicki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorisnickiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
